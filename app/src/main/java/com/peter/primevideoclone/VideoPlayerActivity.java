2918c754d069bd638e22ad25a9118c631ff7bbc9
mwanga <aguthi.aa@gmail.com>
2021-01-01 22:33:12 +0300

    file url

--- app/src/main/java/com/peter/primevideoclone/VideoPlayerActivity.java
+++ app/src/main/java/com/peter/primevideoclone/VideoPlayerActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.SimpleExoPlayer;
@@ -24,6 +26,10 @@ public class VideoPlayerActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_video_player);
 
         videoPlayer = findViewById(R.id.video_player);
